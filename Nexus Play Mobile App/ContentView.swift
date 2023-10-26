57cc16f6afd0b754913b1baf9fb345c9f3252ae7
Jygo547 <jygo3715@colorado.edu>
2023-10-26 13:10:57 -0600

    Initial Commit

---
+++ Nexus Play Mobile App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nexus Play Mobile App
+//
+//  Created by ATLAS Checkout 6 Guest on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nexus Play Mobile App/Nexus_Play_Mobile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nexus_Play_Mobile_AppApp.swift
+//  Nexus Play Mobile App
+//
+//  Created by ATLAS Checkout 6 Guest on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Nexus_Play_Mobile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
